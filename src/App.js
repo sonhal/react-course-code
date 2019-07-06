d44e8bdfed7bca15435739ef51d1f8b6ef89eefb
sondre <sondre.hal@gmail.com>
2019-07-06 23:50:05 +0200

    Remove error boundary wrapping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -42,15 +41,15 @@ class App extends Component {
         persons = (
           <div>
             {this.state.persons.map((person, index) => {
-              return (
-            <ErrorBoundary key={person.id} >
+              return ( 
               <Person 
                 name={person.name} 
                 age={person.age} 
+                key={person.id}
                 click={ () => this.deletePersonHandler(index)}
                 changed={(event) => this.nameChangedHandler(event, person.id)}>
                   {person.text}
-              </Person></ErrorBoundary> )
+              </Person>)
             })}
         </div>
         );
